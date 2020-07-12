cc64beb8d7821f8674ac051825fcce84045ddd45
rufushiyo <67810972+rufushiyo@users.noreply.github.com>
2020-07-12 14:32:11 +0900

    テスト用、全アップ
    練習用

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SubCall</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="heading">Welcome to Subcall</h1>
+        <script>
+          function golink(){
+             location.href="login.php";
+          }
+        </script>
+        <button type="button" onclick="golink()">ログイン</button></br>
+        <form action="login.php" method="post">
+          部屋番号を入力してください
+          <input type="text" name="roomId" size="30" maxlength="20"></br>
+          言語を選択してください(Select a language)
+          <select name="language">
+            <option value="ja">日本語</option>
+            <option value="english">英語</option>
+        </p>
+</body>
+</html>
